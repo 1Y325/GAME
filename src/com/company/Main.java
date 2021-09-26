2baae7da01ce658a4f263843d6cc90f3c05c844f
Yash Gupta <yashg3600@gmail.com>
2021-09-26 21:36:29 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+class employee {
+    int eid;
+    String ename;
+   static String cname;
+    int sal;
+    employee(int a, int S , String name) {
+        eid = a;
+        ename = name;
+        sal = S;
+
+
+    }
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+        //class employee 4 attributes eid , ename , esalasry, companyname,
+        //by using the parametrized constructor initialize , get input from user
+        //employee arr[] = new employee[5];
+        // e class make method result  result method , result is static , highest salary print employee name
+        Scanner S = new Scanner(System.in);
+
+        employee arr[] = new employee[5];
+        for (int i = 0; i <arr.length ; i++) {
+
+
+        }
+
+        }
+    }
+}
